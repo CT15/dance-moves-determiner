d9134e3cc732b9e271ddfd729fd46d4f44762c5f
Calvin Tantio <calvin.tantio@outlook.com>
2018-11-17 21:46:10 +0800

    Remove print in test.py

--- test.py
+++ test.py
@@ -13,12 +13,11 @@ if __name__ == "__main__":
     model = MLModel()
     
     y_pred = model.test_predict(X_test)
-    print(y_pred)
-    print(y_test)
     test_score = accuracy_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred, average='weighted')
 
     print(colored("[EVAL]", "blue"), " f1 = " + str(f1) + "\t\ttest_score = " + str(test_score))
+    
     print(colored("[EVAL]", "blue"), " Generating confusion matrix ...")
 
     cm_df = pd.DataFrame(confusion_matrix(y_test, y_pred))
