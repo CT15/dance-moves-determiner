71e46879eb7ea8f7f76b3b696157f260b7cf1b65
Calvin Tantio <calvin.tantio@outlook.com>
2018-11-18 22:17:30 +0800

    Changed naming: svc to svm

--- train_svm.py
+++ train_svm.py
@@ -17,11 +17,11 @@ def generate_svc(train_df, test_df):
 
     sample_svc = SVC(random_state=40)
 
-    print(colored("[SVC]", "green"), " List of parameters:")
+    print(colored("[SVM]", "green"), " List of parameters:")
     for key in sample_svc.get_params().keys():
         print("\t- " + str(key))
 
-    print(colored("[SVC]", "green"), " Hyperparameters to tune:")
+    print(colored("[SVM]", "green"), " Hyperparameters to tune:")
     for hyperparam in ['degree', 'kernel', 'gamma']:
         print("\t- " + hyperparam)
 
@@ -38,7 +38,7 @@ def generate_svc(train_df, test_df):
                    'kernel': kernel,
                    'gamma': gamma}
 
-    print(colored("[SVC]", "green"), " Random grid created:")
+    print(colored("[SVM]", "green"), " Random grid created:")
     file.write("Random Grid for Random Search:\n")
     for key in random_grid.keys():
         to_print = "\t- " + key + ":\t" + str(random_grid[key])
@@ -47,7 +47,7 @@ def generate_svc(train_df, test_df):
 
     file.write("\n")
 
-    print(colored("[SVC]", "green"),  " Instatiating Random Search ...")
+    print(colored("[SVM]", "green"),  " Instatiating Random Search ...")
 
     # Base model to tune
     clf = SVC()
@@ -56,10 +56,10 @@ def generate_svc(train_df, test_df):
     X_train, y_train = prep.get_X_y(train_df)
     X_test, y_test = prep.get_X_y(test_df)
 
-    print(colored("[SVC]", "green"), " Start Random Search fitting ...")
+    print(colored("[SVM]", "green"), " Start Random Search fitting ...")
     clf_random.fit(X_train, y_train)
 
-    print(colored("[SVC]", "green"), " Random Search fitting DONE with best hyperparameters:")
+    print(colored("[SVM]", "green"), " Random Search fitting DONE with best hyperparameters:")
     file.write("Best hyperparameters from Random Search:\n")
 
     bp = clf_random.best_params_
@@ -70,18 +70,18 @@ def generate_svc(train_df, test_df):
 
     file.write("\n")
 
-    print(colored("[SVC]", "green"), " Initializing base model ...")
+    print(colored("[SVM]", "green"), " Initializing base model ...")
 
     base_model = SVC(degree=bp['degree'],
                   kernel=bp['kernel'],
                                         gamma=bp['gamma'])
     base_model.fit(X_train, y_train)
 
-    print(colored("[SVC]", "green"), " Evaluating base model ...")
+    print(colored("[SVM]", "green"), " Evaluating base model ...")
     base_score = evaluate(base_model, X_train, y_train, X_test, y_test, 'SVC', 1)
     file.write("Base model evaluation: [refer to svc_eval1.txt]\n\n")
 
-    print(colored("[SVC]", "green"), " Creating parameter grid for grid search ...")
+    print(colored("[SVM]", "green"), " Creating parameter grid for grid search ...")
 
     # Number of trees in random forest
     degree = [bp['degree'],bp['degree']+1]
@@ -94,7 +94,7 @@ def generate_svc(train_df, test_df):
                   'kernel': kernel,
                   'gamma': gamma}
 
-    print(colored("[SVC]", "green"), " Random grid created:")
+    print(colored("[SVM]", "green"), " Random grid created:")
     file.write("Random Grid for Grid Search:\n")
     for key in param_grid.keys():
         to_print = "\t- " + key + ":\t" + str(param_grid[key])
@@ -108,10 +108,10 @@ def generate_svc(train_df, test_df):
 
     clf_grid = GridSearchCV(estimator=clf, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
 
-    print(colored("[SVC]", "green"), " Start Grid Search fitting ...")
+    print(colored("[SVM]", "green"), " Start Grid Search fitting ...")
     clf_grid.fit(X_train, y_train)
 
-    print(colored("[SVC]", "green"), " Grid Search fitting DONE with best hyperparameters:")
+    print(colored("[SVM]", "green"), " Grid Search fitting DONE with best hyperparameters:")
     file.write("Best hyperparameters from Grid Search:\n")
     bp = clf_grid.best_params_
     for key in bp.keys():
@@ -121,36 +121,36 @@ def generate_svc(train_df, test_df):
 
     file.write("\n")
 
-    print(colored("[SVC]", "green"), " Initializing best grid model ...")
+    print(colored("[SVM]", "green"), " Initializing best grid model ...")
     grid_model = SVC(degree=bp['degree'],
                      kernel=bp['kernel'],
                                         gamma=bp['gamma'])
 
     grid_model.fit(X_train, y_train)
 
-    print(colored("[SVC]", "green"), " Evaluating grid model ...")
+    print(colored("[SVM]", "green"), " Evaluating grid model ...")
     grid_score = evaluate(grid_model, X_train, y_train, X_test, y_test, 'SVC', 2)
     file.write("Fine tuned model evaluation: [refer to svc_eval2.txt]\n\n")
 
     improvement = 100 * (grid_score - base_score) / base_score
 
-    print(colored("[SVC]", "green"), " Improvement of " + str(improvement) + "% from the base model")
+    print(colored("[SVM]", "green"), " Improvement of " + str(improvement) + "% from the base model")
     file.write("Improvement of " + str(improvement) + "% from the base model\n\n")
 
     if improvement <= 0:
         to_print = "Base model is used"
-        print(colored("[SVC]", "green"), " " + to_print)
+        print(colored("[SVM]", "green"), " " + to_print)
         file.write(to_print + "\n")
         clf = base_model
     else:
         to_print = "Grid model is used"
-        print(colored("[SVC]", "green"), " " + to_print)
+        print(colored("[SVM]", "green"), " " + to_print)
         file.write(to_print + "\n")
         clf = grid_model
 
-    print(colored("[SVC]", "green"), " Saving SVCmodel ...")
+    print(colored("[SVM]", "green"), " Saving SVCmodel ...")
 
-    with open('./models/svc.sav', 'wb') as f:
+    with open('./models/svm.sav', 'wb') as f:
         pickle.dump(clf, f)
 
     print(colored("[RF]", "green"), " SVC model saved")
