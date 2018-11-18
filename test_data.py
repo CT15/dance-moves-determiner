c89cdd6c68e766eba390fc816685de4e832fad30
Calvin Tantio <calvin.tantio@outlook.com>
2018-11-18 16:39:55 +0800

    test_data.py: load test data

--- test_data.py
+++ test_data.py
@@ -8,7 +8,7 @@ import preprocess as prep
 # SideStep ==> 3        # Swing ==> 8
 # TurnClap ==> 4        # Cowboy ==> 9
 def prepare_data(csv_name, number, interval=60):
-    df = pd.read_csv(sys.path[0] + '/train_data/' + csv_name + '.csv', header=None)
+    df = pd.read_csv(sys.path[0] + '/test_data/' + csv_name + '.csv', header=None)
 #    df = preprocess.normalise(df)
     df_max_min = prep.flatten(df, interval)
     df_var = prep.flatten(df, 'var', interval)
@@ -25,20 +25,18 @@ def load(with_names=False):
     sarah_number7_test = prepare_data('sarah_number7_test', 1)
     sarah_chicken_test = prepare_data('sarah_chicken_test', 2)
     sarah_sidestep_test = prepare_data('sarah_sidestep_test', 3)
-    sarah_turnclap1_test = prepare_data('sarah_turnclap1_1min', 4)
-    sarah_turnclap2_test = prepare_data('sarah_turnclap2_1min', 4)
+    sarah_turnclap1_test = prepare_data('sarah_turnclap_test', 4)
     sarah_number6_test = prepare_data('sarah_number6_test', 5)
     sarah_salute_test = prepare_data('sarah_salute_test', 6)
     sarah_mermaid_test = prepare_data('sarah_mermaid_test', 7)
     sarah_swing_test = prepare_data('sarah_swing_test', 8)
-    sarah_cowboy1_test = prepare_data('sarah_cowboy1_1min', 9)
-    sarah_cowboy2_test = prepare_data('sarah_cowboy2_1min', 9)
+    sarah_cowboy1_test = prepare_data('sarah_cowboy_test', 9)
     sarah_finalmove_test = prepare_data('sarah_finalmove_test', 10)
 
     all_tests.extend([
         sarah_wipers_test, sarah_number7_test, sarah_chicken_test, sarah_sidestep_test,
-        sarah_turnclap1_test, sarah_turnclap2_test, sarah_number6_test, sarah_salute_test,
-        sarah_mermaid_test, sarah_swing_test, sarah_cowboy1_test, sarah_cowboy2_test,
+        sarah_turnclap_test, sarah_number6_test, sarah_salute_test,
+        sarah_mermaid_test, sarah_swing_test, sarah_cowboy_test,
         sarah_finalmove_test
     ])
 
@@ -46,8 +44,8 @@ def load(with_names=False):
         all_names = []
         all_names.extend([
             'sarah_wipers_test', 'sarah_number7_test', 'sarah_chicken_test', 'sarah_sidestep_test',
-            'sarah_turnclap1_test', 'sarah_turnclap2_test', 'sarah_number6_test', 'sarah_salute_test',
-            'sarah_mermaid_test', 'sarah_swing_test', 'sarah_cowboy1_test', 'sarah_cowboy2_test',
+            'sarah_turnclap_test', 'sarah_number6_test', 'sarah_salute_test',
+            'sarah_mermaid_test', 'sarah_swing_test', 'sarah_cowboy_test',
             'sarah_finalmove_test'
         ])
         return (all_tests, all_names)
